e3726db5206adf8b873fceee81bb568d80ed50aa
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-01 09:58:06 -0700

    manipulated the hell out that DOM

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Marcus is the champion";
